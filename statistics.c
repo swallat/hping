b0ffd000c5c3a37fc30b2f0f298b537cb6fb8788
Sebastian Wallat <sebastian@wallat.de>
2012-11-16 16:39:08 +0100

    corrected misspelling in statistic

--- statistics.c
+++ statistics.c
@@ -83,7 +83,7 @@ void	print_statistics(int signal_id)
 	}
 
 	fprintf(stderr, "\n--- %s hping statistic ---\n", targetname);
-	fprintf(stderr, "%d packets tramitted, %d packets received, "
+	fprintf(stderr, "%d packets transmitted, %d packets received, "
 			"%d%% packet loss\n", sent_pkt, recv_pkt, lossrate);
 	if (out_of_sequence_pkt)
 		fprintf(stderr, "%d out of sequence packets received\n",
